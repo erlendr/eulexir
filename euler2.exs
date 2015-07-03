9a87dccfbe8f876b8fd2967c40cf2e1ea41d2f24
Erlend Røsjø <erlend@erlend.net>
2015-07-03 12:16:25 +0200

    Problem 2 solved using streams instead

--- euler2.exs
+++ euler2.exs
@@ -1,16 +1,2 @@
-defmodule Fib do
-  def fib(a, b, list) do
-    num = a + b
-    if num > 400_000_0 do
-      list
-    else
-      res = list ++ [num]
-      fib(b, num, res)
-    end
-  end
-end
-
-start = [1,2]
-
-result = Fib.fib(1, 2, start) |> Enum.filter &(rem(&1, 2) == 0)
+result = Stream.unfold({0,1}, fn 0 -> nil; n -> { elem(n, 0) + elem(n, 1), {elem(n, 1), elem(n, 0) + elem(n, 1)}} end) |> Enum.take_while( &(&1 < 4000000)) |> Enum.filter (fn(x) -> rem(x,2) == 0 end)
 IO.puts Enum.sum(result)
